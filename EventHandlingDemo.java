3974378d346974cbecf84aed0025356d36ab06a5
Atharv002 <damleatharv02@gmail.com>
2023-06-01 20:25:18 +0530

    commit

--- EventHandlingDemo.java
+++ EventHandlingDemo.java
@@ -1,6 +1,5 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 
 public class EventHandlingDemo implements ActionListener, MouseListener, MouseMotionListener {
     private JFrame frame;
