4b454efb967de49c08da7add82751e8ac177fb3b
Atharv002 <damleatharv02@gmail.com>
2023-06-03 10:34:44 +0530

    commit

---
+++ Rectangle.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Rectangle {
+    public static void main(String [] args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter the breadth of rectangle: ");
+        int b=sc.nextInt();
+        System.out.println("Enter the length of rectangle: ");
+        int l=sc.nextInt();
+
+        for(int i=0;i<l;i++){
+            for(int j=0;j<b;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        sc.close();
+    }    
+}
+
+
+
+
+
