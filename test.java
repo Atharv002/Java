848e9d5c0cc155b38fb71a17023ce08fdc862760
Atharv002 <damleatharv02@gmail.com>
2023-06-04 10:19:18 +0530

    commit

---
+++ test.java
@@ -0,0 +1,12 @@
+import java.io.*;
+
+class test{
+    public static void main(String[] args) throws Exception{
+        FileReader fr=new FileReader("./file.txt");    
+        BufferedReader br=new BufferedReader(fr);    
+        int i;    
+        while((i=br.read())!=-1){  
+            System.out.print((char)i);  
+        }
+    }
+}
