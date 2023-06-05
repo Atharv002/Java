8584b730ea314f537792afd9b64a3778258fec18
Atharv002 <damleatharv02@gmail.com>
2023-06-05 10:06:14 +0530

    commit

---
+++ superKW.java
@@ -0,0 +1,29 @@
+class Animal{
+    public void greet(){
+        System.out.println("Greet");
+    }
+}
+
+class Dog extends Animal{
+    public void greet(){
+        System.out.println("Barking123");
+    }
+    public void greet2(){
+        super.greet();
+    }
+}
+
+class Puppy extends Dog{
+    public void wheeze(){
+        System.out.println("Wheezing");
+    }
+}
+
+
+public class superKW {
+    public static void main(String args[]){
+        Puppy p = new Puppy();
+        p.greet();
+        p.greet2();
+    }
+}
