b09e5495339a52d23789ab612535727994b319b6
Atharv002 <damleatharv02@gmail.com>
2023-06-05 10:08:42 +0530

    commit

---
+++ packages/Calculator.java
@@ -0,0 +1,10 @@
+package packages;
+
+public class Calculator {
+    public static int add(int a, int b){
+        return (a+b);
+    }
+    public static int sub(int a, int b){
+        return (a-b);
+    }
+}
