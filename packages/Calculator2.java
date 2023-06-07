d7cddd84832522476c9985feb1bb4ef1b374e3bb
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:22:36 +0530

    commit

--- packages/Calculator2.java
+++ packages/Calculator2.java
@@ -2,7 +2,10 @@ package packages;
 
 public class Calculator2 {
     public static int mod(int a,int b){
-        if(b==0){return -1;}
+        if(b==0){
+            System.out.println("Division by zero error");
+            return -1;
+        }
         return a%b;
     }
 }
