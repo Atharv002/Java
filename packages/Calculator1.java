61ee27723533df5fdf2ea660d82251a1947c8fee
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:18:10 +0530

    commit

--- packages/Calculator1.java
+++ packages/Calculator1.java
@@ -4,20 +4,4 @@ public class Calculator1 extends Exception{
     public static int add(int a, int b){
         return (a+b);
     }
-    public static int sub(int a, int b){
-        return (a-b);
-    }
-    public static int mul(int a,int b){
-        return a*b;
-    }
-    public static int div(int a,int b){
-        try{
-            return a/b;
-        }
-        catch (Exception e){
-            System.out.println("Error detected");
-            e.printStackTrace();
-            return -1;
-        }
-    }
 }
