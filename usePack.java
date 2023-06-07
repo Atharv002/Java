d121f885688cc173e443cda351e42695cfd38c27
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:38:23 +0530

    commit

--- usePack.java
+++ usePack.java
@@ -4,5 +4,6 @@ public class usePack{
     public static void main(String args[]) {
         System.out.println(packages.Calculator1.add(1,2));
         System.out.println(packages.Calculator1.sub(3,4));
+        System.out.println(packages.Calculator1.mul(3,4));
     }
 }
