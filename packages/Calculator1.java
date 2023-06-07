6e2516739c8de100d7212ae9f214e57fbbf3e4c7
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:19:11 +0530

    commit

--- packages/Calculator1.java
+++ packages/Calculator1.java
@@ -3,7 +3,7 @@ package packages;
 public class Calculator1 extends Exception{
     public static int add(int a, int b){
         int ans=a+b;
-        return sum;
+        return ans;
     }
     public static int sub(int a, int b){
         int ans=a-b;
