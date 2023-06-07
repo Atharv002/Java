c285e829fcb5c3cf0d3918b092cadf3ea9c1f983
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:37:07 +0530

    commit

--- usePack.java
+++ usePack.java
@@ -2,15 +2,6 @@ import packages.*;
 
 public class usePack{
     public static void main(String args[]) {
-        int val = Calculator.add(1,2);
-        System.out.println(val);
-        int val2 = packages.Calculator2.mul(3,2);
-        System.out.println(val2);
-        int val3=packages.Calculator.sub(2,1);
-        System.out.println(val3);
-        int val4=packages.Calculator.mul(5,5);
-        System.out.println(val4);
-        int val5=packages.Calculator2.mod(35,3);
-        System.out.println(val5);
+        System.out.println(packages.Calculator1.add(1,2));
     }
 }
