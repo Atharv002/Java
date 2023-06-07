6d3615345be35ddcaf022e9116c1623e9fb4c949
Atharv002 <damleatharv02@gmail.com>
2023-06-07 09:21:28 +0530

    commit

--- packages/Calculator2.java
+++ packages/Calculator2.java
@@ -1,21 +1,5 @@
 package packages;
 
 public class Calculator2 {
-    public static int mod(int a,int b){
-        if(b==0){
-            System.out.println("Division by zero error");
-            return -1;
-        }
-        return (a%b);
-    }
-    public static int power(int a, int b){
-        int sum=1;
-        for(int i=1;i<=b;i++){
-            sum*=a;
-        }
-        return sum;
-    }
-    public static int mul(int a, int b){
-        return (a*b);
-    }
+    
 }
